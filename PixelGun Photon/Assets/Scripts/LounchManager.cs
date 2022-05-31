0a7d0c1364ca609a78b4497ea898708824bff73c
KrisMultiDev <krissaran2003@gmail.com>
2022-05-31 23:08:32 +0300

    start

---
+++ PixelGun Photon/Assets/Scripts/LounchManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class LounchManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+    }
+
+    public override void OnConnected()
+    {
+        
+    }
+}
